3cccf0d0ea1fd400e6357f9ac972f5d35350c35f
jorfsson <35546435+jorfsson@users.noreply.github.com>
2018-02-28 15:38:39 -0800

    Added Named Range specification

--- spreadsheet-parser.js
+++ spreadsheet-parser.js
@@ -12,12 +12,31 @@ function logProductInfo() {
     approved: "Approved QA Complete"
   }
 
-  for (var i = 0; i < data.length; i++) {
-    var status = data[i];
 
-    if (!!(status[4] == STATUS_MSG.ready)) {
-    Logger.log(status[4] + " " + status[5]);
+  // namedRangeList {array} - delcares an array of all the named ranges in the spreadsheet
+  // >[NamedRange, NamedRange, NamedRange...]
+  var namedRangeList = sheet.getNamedRanges();
+  var i = 0;
+  //for (var i = 0; i < namedRangeList.length - 1; i++) {
 
-    }
-  }
+    // range {object} - declares a variable for a named range
+     var range = namedRangeList[i];
+
+     // rangeName {string} - returns the name of a selected range
+     var rangeName = range.getName();
+
+     // datarange {Object[][]} - declares a variable for a rectangular object containing all values
+       // within object range (which has an object type of range, unique to Google Sheets Script)
+     // datarange is now an instance that can have range class methods used on it
+     var dataRange = range.getRange().getValues();
+
+     for (var a = 0; a < dataRange.length; a++) {
+       var status = dataRange[a];
+
+       if (!!(status[4] == STATUS_MSG.ready)) {
+         Logger.log(status[5] + " " + status[5]);
+       }
+
+
+     }
 }
