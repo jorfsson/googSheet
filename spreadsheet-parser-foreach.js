be39d3679ed0914b5f5a4605c8be71be980250fb
jorfsson <35546435+jorfsson@users.noreply.github.com>
2018-03-01 11:21:49 -0800

    Cleaned up code

--- spreadsheet-parser-foreach.js
+++ spreadsheet-parser-foreach.js
@@ -12,34 +12,23 @@ function logProductInfo() {
     approved: "Approved QA Complete"
   }
 
-
   // namedRangeList {array} - delcares an array of all the named ranges in the spreadsheet
   // >[NamedRange, NamedRange, NamedRange...]
   var namedRangeList = sheet.getNamedRanges();
 
   namedRangeList.forEach(function(namedRange){
-  //for (var i = 0; i < namedRangeList.length - 1; i++) {
-
-     //var range = namedRangeList[i];
-     //var range = namedRange.getRange();
-     var rangeName = namedRange.getName();
-
-     // datarange {Object[][]} - declares a variable for a rectangular object containing all values
-       // within object range (which has an object type of range, unique to Google Sheets Script)
-     // datarange is now an instance that can have range class methods used on it
+    var rangeName = namedRange.getName();
 
     try {
      var dataRange = namedRange.getRange().getValues();
-     dataRange.forEach(function(row) {
-     //for (var a = 0; a < dataRange.length; a++) {
-      // var status = dataRange[a];
 
-       if (!!(row[4] == STATUS_MSG.approved)) {
+     dataRange.forEach(function(row) {
+      if (!!(row[4] == STATUS_MSG.approved)) {
          Logger.log("Division Page: " + rangeName + "\n Current Status is set to: " + row[4] + "\nContent Name: " + row[5]);
        }
      });
-   } catch (err) {
+    } catch (err) {
      Logger.log(err);
    }
-  });
+ });
 }
